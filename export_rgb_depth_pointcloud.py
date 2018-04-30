7e31fa018fb1d21b527f96ff2fb452f621589a77
Jan Bednar <bednaj14@fel.cvut.cz>
2018-04-30 17:09:43 +0200

    Added export of K matrices

--- export_rgb_depth_pointcloud.py
+++ export_rgb_depth_pointcloud.py
@@ -8,6 +8,8 @@ while not turtle.is_shutting_down():
     depth = turtle.get_depth_image()
     rgb = turtle.get_rgb_image()
     point_cloud = turtle.get_point_cloud()
+    rgb_K = turtle.get_rgb_K()
+    depth_K = turtle.get_depth_K()
 
     if rgb is None:
         continue
@@ -15,9 +17,15 @@ while not turtle.is_shutting_down():
         continue
     if point_cloud is None:
         continue
+    if rgb_K is None:
+        continue
+    if depth_K is None:
+        continue
 
     np.save('exports/depth.npy', depth)
     np.save('exports/rgb.npy', rgb)
     np.save('exports/point_cloud.npy', point_cloud)
+    np.save('exports/rgb_K.npy', rgb_K)
+    np.save('exports/depth_K.npy', depth_K)
 
     exit(0)
