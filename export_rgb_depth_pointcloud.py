79b8ebdb4c23d8112055e3c9b47e0b7ab8e1ac4e
Jan Bednar <bednaj14@fel.cvut.cz>
2018-04-30 17:02:34 +0200

    added folder to save data

--- export_rgb_depth_pointcloud.py
+++ export_rgb_depth_pointcloud.py
@@ -9,11 +9,15 @@ while not turtle.is_shutting_down():
     rgb = turtle.get_rgb_image()
     point_cloud = turtle.get_point_cloud()
 
-    if rgb or depth or point_cloud is None:
+    if rgb is None:
+        continue
+    if depth is None:
+        continue
+    if point_cloud is None:
         continue
 
-    np.save('depth.npy', depth)
-    np.save('rgb.npy', rgb)
-    np.save('point_cloud.npy', point_cloud)
+    np.save('exports/depth.npy', depth)
+    np.save('exports/rgb.npy', rgb)
+    np.save('exports/point_cloud.npy', point_cloud)
 
     exit(0)
