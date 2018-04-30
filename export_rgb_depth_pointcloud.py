b2c7579fb1fb0b40da28e493aadcca7a702cded4
Jan Bednar <bednaj14@fel.cvut.cz>
2018-04-30 16:46:50 +0200

    fixed problem with getting data

--- export_rgb_depth_pointcloud.py
+++ export_rgb_depth_pointcloud.py
@@ -1,11 +1,19 @@
 from turtlebot import Turtlebot
-impot numpy as np
+import numpy as np
 
-turtle = Turtlebot()
-depth = turtle.get_depth_image()
-rgb = turtle.get_rgb_image()
-point_cloud = turtle.get_point_cloud()
+turtle = Turtlebot(pc=True, rgb=True, depth=True)
 
-np.save('depth.npy', depth)
-np.save('rgb.npy', rgb)
-np.save('point_cloud.npy', point_cloud)
+while not turtle.is_shutting_down():
+
+    depth = turtle.get_depth_image()
+    rgb = turtle.get_rgb_image()
+    point_cloud = turtle.get_point_cloud()
+
+    if rgb or depth or point_cloud is None:
+        continue
+
+    np.save('depth.npy', depth)
+    np.save('rgb.npy', rgb)
+    np.save('point_cloud.npy', point_cloud)
+    
+    exit(0)
