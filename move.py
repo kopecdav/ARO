aaf560a0eb449a8e9f5a4f26e8d27065e989478e
David Kopecky <kopecdav@fel.cvut.cz>
2018-05-05 13:26:18 +0200

    Add traslate function

--- move.py
+++ move.py
@@ -7,8 +7,6 @@ def rotate(turtle = None, angle=0,speed = 1, verbose = False):
     rate = Rate(10)
 
     movement_time = abs(angle)/speed
-
-
     moved_angle=0
 
     turtle.cmd_velocity(linear=0, angular=0)
@@ -26,16 +24,47 @@ def rotate(turtle = None, angle=0,speed = 1, verbose = False):
     t = get_time()
     while abs(moved_angle) < abs(angle + off - corection):
 
-	print("moved_angle: " + str(moved_angle))
-	[x,y,a] = turtle.get_odometry()
-	print("odometry: " + str(a))
-	moved_angle= moved_angle + a
-	turtle.reset_odometry()
-	turtle.cmd_velocity(linear=0, angular=speed)
+	    print("moved_angle: " + str(moved_angle))
+	    [x,y,a] = turtle.get_odometry()
+	    print("odometry: " + str(a))
+	    moved_angle= moved_angle + a
+	    turtle.reset_odometry()
+	    turtle.cmd_velocity(linear=0, angular=speed)
         print("Angle : " + str(moved_angle))
-	rate.sleep()
+	    rate.sleep()
 
     turtle.cmd_velocity(linear=0,angular=0)
     if verbose:
 	    print("rotation done")
+
+
+def translate(turtle = None, linear=2, speed = 1, verbose = False):
+
+    corection = 0
+    rate = Rate(10)
+
+    movement_time = abs(angle) / speed
+    moved_angle = 0
+
+    turtle.cmd_velocity(linear=0, angular=0)
+    turtle.reset_odometry()
+    _, _, off = turtle.get_odometry()
+
+    if verbose:
+        print("Translating for : " + str(movement_time) + " seconds")
+        print("Offset:" + str(off))
+
+    if linear < 0:
+        speed = -speed
+        corection = -corection
+
+    t = get_time()
+    moved_distance = 0
+
+    while abs(moved_distance) < abs(linear + off - corection):
+
+        print("Something gonna happen here soon")
+
+
+
 
